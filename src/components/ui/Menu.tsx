032e2e8e1e1c5b77de9ff45c14ccab7ed5415693
jaha-sudo <romanovajaha@gmail.com>
2023-08-17 17:12:03 +0500

    commented (temporarily) footer in dialog menu with version (because outputs my branche's name)

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -165,7 +165,7 @@ const Menu: FC<OwnProps> = ({
         onClick={autoClose ? onClose : undefined}
       >
         {children}
-        {footer && <div className="footer">{footer}</div>}
+        {/* {footer && <div className="footer">{footer}</div>} */}
       </div>
     </div>
   );
